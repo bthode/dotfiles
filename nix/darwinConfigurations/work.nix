3f59e9d07880948c2cc240dfe48254e8b3e7a389
Bryan Thode <bryan@tatq.com>
2025-09-16 08:27:59 -0500

    Move stateVersion value into config decl

--- nix/darwinConfigurations/work.nix
+++ nix/darwinConfigurations/work.nix
@@ -29,9 +29,9 @@ let
             enable = true;
           };
         };
-      };
-      system = {
-        stateVersion = 6;
+        system = {
+          stateVersion = 6;
+        };
       };
     };
 in
